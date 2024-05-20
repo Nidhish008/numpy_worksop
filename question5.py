a26628396622302aacb40466adb228419a82424f
Nidhish <nidhish8102005@gmail.com>
2024-05-20 14:40:02 +0530

    Saved

--- question5.py
+++ question5.py
@@ -1,1 +1,12 @@
 #write a program to find if the given number is prime no or not
+
+n=int(input("Enter the no:"))
+count=0
+for i in range(2,n):
+    if(n%i==0):
+        count+=1
+if(count>=1):
+    print("Not a prime no")
+else:
+    print("Prime no")
+
--- question6.py
+++ question6.py
@@ -1,1 +1,10 @@
 #write a program to find the maximum of two numbers
+def max(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+
+n1=int(input("Enter the first number:"))
+n2=int(input("Enter the second number:"))
+print(max(n1,n2))
